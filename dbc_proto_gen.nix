bcdfebf5409ba700691e19f4951722f45b72d1da
Ben Hall <rcmast3r1@gmail.com>
2023-12-20 18:36:14 -0500

    working on ci

--- dbc_proto_gen.nix
+++ dbc_proto_gen.nix
@@ -4,7 +4,7 @@ python311Packages.buildPythonApplication {
   pname = "py_dbc_proto_gen";
   version = "1.0.0";
 
-
+  
   propagatedBuildInputs = [ python311Packages.cantools python311Packages.protobuf python311Packages.requests ];
 
   src = ./py_dbc_proto_gen;
