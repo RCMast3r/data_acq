378901483dc2b6a27d7637c1cb6ac149dbe5c620
Ben Hall <rcmast3r1@gmail.com>
2024-03-18 12:57:39 -0400

    modernized flake

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     utils.url = "github:numtide/flake-utils";
     mcap-protobuf.url = "github:RCMast3r/mcap-protobuf-support-flake";
+    flake-utils.url = "github:numtide/flake-utils";
     mcap.url = "github:RCMast3r/py_mcap_nix";
     foxglove-websocket.url = "github:RCMast3r/py_foxglove_webserver_nix";
     asyncudp.url = "github:RCMast3r/asyncudp_nix";
@@ -13,8 +14,20 @@
     nix-proto = { url = "github:notalltim/nix-proto"; };
   };
 
-  outputs = { self, nixpkgs, utils, mcap-protobuf, mcap, foxglove-websocket
-    , asyncudp, nix-proto, ht_can_pkg_flake, ... }@inputs:
+  outputs =
+    { self
+    , nixpkgs
+    , utils
+    , mcap-protobuf
+    , mcap
+    , foxglove-websocket
+    , asyncudp
+    , nix-proto
+    , ht_can_pkg_flake
+    , flake-utils
+    , ...
+    }@inputs:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-darwin" "x86_64-darwin" "aarch64-linux" ] (system:
     let
       makePackageSet = pkgs: {
         py_data_acq_pkg = pkgs.py_data_acq_pkg;
@@ -54,38 +67,14 @@
         asyncudp.overlays.default
         foxglove-websocket.overlays.default
       ] ++ nix-proto.lib.overlayToList nix_protos_overlays;
-      system = builtins.currentSystem;
-      x86_pkgs = import nixpkgs {
-        system = "x86_64-linux";
-        # inherit system;
-        # system = builtins.currentSystem;
-        overlays = [ self.overlays.default ]
-          ++ nix-proto.lib.overlayToList nix_protos_overlays;
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = my_overlays;
       };
 
-      arm_pkgs = import nixpkgs {
-        system = "aarch64-linux";
-        # inherit system;
-        # system = builtins.currentSystem;
-        overlays = [ self.overlays.default ]
-          ++ nix-proto.lib.overlayToList nix_protos_overlays;
-      };
-
-      packageSets = {
-        "x86_64-linux" = makePackageSet x86_pkgs;
-        "aarch64-linux" = makePackageSet arm_pkgs;
-        # Add more systems as needed
-      };
-    in {
-
-      overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
-
-      packages = packageSets;
-
-      devShells.x86_64-linux.default = x86_pkgs.mkShell rec {
-        # Update the name to something that suites your project.
+      shared_shell = pkgs.mkShell rec {
         name = "nix-devshell";
-        packages = with x86_pkgs; [
+        packages = with pkgs; [
           jq
           py_data_acq_pkg
           py_dbc_proto_gen_pkg
@@ -97,22 +86,24 @@
         ];
         # Setting up the environment variables you need during
         # development.
-        shellHook = let icon = "f121";
-        in ''
-          path=${x86_pkgs.proto_gen_pkg}
-          bin_path=$path"/bin"
-          dbc_path=${x86_pkgs.ht_can_pkg}
-          export BIN_PATH=$bin_path
-          export DBC_PATH=$dbc_path
+        shellHook =
+          let icon = "f121";
+          in ''
+            path=${pkgs.proto_gen_pkg}
+            bin_path=$path"/bin"
+            dbc_path=${pkgs.ht_can_pkg}
+            export BIN_PATH=$bin_path
+            export DBC_PATH=$dbc_path
 
-          echo -e "PYTHONPATH=$PYTHONPATH\nBIN_PATH=$bin_path\nDBC_PATH=$dbc_path\n" > .env
-          export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
-        '';
+            echo -e "PYTHONPATH=$PYTHONPATH\nBIN_PATH=$bin_path\nDBC_PATH=$dbc_path\n" > .env
+            export PS1="$(echo -e '\u${icon}') {\[$(tput sgr0)\]\[\033[38;5;228m\]\w\[$(tput sgr0)\]\[\033[38;5;15m\]} (${name}) \\$ \[$(tput sgr0)\]"
+          '';
       };
-      devShells.x86_64-linux.ci = x86_pkgs.mkShell rec {
+
+      ci_shell = pkgs.mkShell rec {
         # Update the name to something that suites your project.
         name = "nix-devshell";
-        packages = with x86_pkgs; [
+        packages = with pkgs; [
           # Development Tools
           py_dbc_proto_gen_pkg
           proto_gen_pkg
@@ -120,15 +111,31 @@
           protobuf
         ];
         shellHook =
-        ''
-          path=${x86_pkgs.proto_gen_pkg}
-          bin_path=$path"/bin"
-          dbc_path=${x86_pkgs.ht_can_pkg}
-          export BIN_PATH=$bin_path
-          export DBC_PATH=$dbc_path
-        '';
+          ''
+            path=${pkgs.proto_gen_pkg}
+            bin_path=$path"/bin"
+            dbc_path=${pkgs.ht_can_pkg}
+            export BIN_PATH=$bin_path
+            export DBC_PATH=$dbc_path
+          '';
 
       };
+    in
+    {
 
-    };
+      overlays.default = nixpkgs.lib.composeManyExtensions my_overlays;
+      devShells = {
+        default = shared_shell;
+        ci = ci_shell;
+      };
+
+      packages = rec {
+        default = pkgs.py_data_acq_pkg;
+        py_dbc_proto_gen_pkg = pkgs.py_data_acq_pkg;
+        proto_gen_pkg = pkgs.proto_gen_pkg;
+        hytech_np = pkgs.hytech_np;
+        hytech_np_proto_py = pkgs.hytech_np_proto_py;
+      };
+
+    });
 }
