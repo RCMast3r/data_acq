528121ce4dcad5ff959ca2584a374db5ab2f8fe7
Ben Hall <rcmast3r1@gmail.com>
2024-03-27 03:04:08 -0400

    kinda fixed build, paths not correct in output

--- frontend.nix
+++ frontend.nix
@@ -23,28 +23,15 @@ in
       src = ./frontend; #gitignore.lib.gitignoreSource ./.; # uses the gitignore in the repo to only copy files git would see
       buildInputs = [ pkgs.nodejs ];
       buildPhase = ''
-        runHook preBuild
         export HOME=$TMP
         
         ln -s ${generated.nodeDependencies}/lib/node_modules ./node_modules
         export PATH="${generated.nodeDependencies}/bin:$PATH"
         npm run build
-        runHook postBuild
       '';
       installPhase = ''
-        runHook preInstall
-            
-        mkdir -p $out/bin
-        # copy only whats needed for running the built app
-        cp package.json $out/package.json
-        cp -r dist $out/dist
-        ln -sf ${generated.nodeDependencies}/lib/node_modules $out/node_modules
-        # copy entry point, in this case our index.ts has the node shebang
-        # nix will patch the shebang to be the node version specified in buildInputs
-        # you could also copy in a script that is basically `npm run start`
-        cp dist/index.js $out/bin/frontend
-        chmod a+x $out/bin/frontend
-        runHook postInstall
+        ls
+        cp -r build $out/
       '';
     };
 }
