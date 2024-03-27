b1c76658babe27bd12193411ba11d03ec914d2b8
BANANAPEEL202 <bbob52645@gmail.com>
2024-03-27 09:28:34 -0400

    tried adding footer

--- py_data_acq/mcap-recovery.py
+++ py_data_acq/mcap-recovery.py
@@ -2,5 +2,18 @@
 
 
 def recover(path):
-    file = open("path", "rb")
+    with open(path, "ab") as file:
+        file.write(b'\x02\x00\x00\x00\x00\x00\x00\x00\x00')
+        file.write(b'\x00\x00\x00\x00\x00\x00\x00\x00')
+        file.write(b'\x00\x00\x00\x00\x02')
+        file.write(b'\x89MCAP0\r\n')
+
+def readMCAP(path):
+    file = open(path, "rb")
     binary_data = file.read()
+    print(binary_data[-30:])
+
+path = '/Users/home/Downloads/recordings/07_28_2023_11_55_03.mcap'
+#readMCAP('/Users/home/Downloads/03_05_2024_23_10_23_V3_V4.mcap')
+recover(path)
+readMCAP(path)
