b8ed2e76951b477ccd3f431506c0e6fe9b348e07
TanglyEagle7718 <rishi.borra@outlook.com>
2024-03-28 19:06:54 -0400

    Allows car send files to base station

--- py_data_acq/py_data_acq/web_server/mcap_server.py
+++ py_data_acq/py_data_acq/web_server/mcap_server.py
@@ -77,6 +77,7 @@ class MCAPServer:
         @app.route('/offload', methods=['POST'])
         def offload_data():
             # os.system("rsync -a ~/recordings urname@192.168.1.101:~/destination/of/data")
+		os.system("./startMCAPTransfer.sh")
             return jsonify()
 
         return app
