dd434ac442e9879dd09ee06c03f7f67a99e31b8a
Ben Hall <rcmast3r1@gmail.com>
2024-03-29 17:11:44 -0400

    um

--- py_data_acq/runner.py
+++ py_data_acq/runner.py
@@ -180,7 +180,8 @@ async def run(logger):
     # and schema in the foxglove websocket server.
 
     # await asyncio.gather(receiver_task, fx_task, mcap_task, srv_task, vn_receiver_task)
-    await asyncio.gather(vn_receiver_task)
+    await asyncio.gather(receiver_task, fx_task, mcap_task, srv_task, vn_receiver_task)
+    # await asyncio.gather(vn_receiver_task)
 
 
 if __name__ == "__main__":
