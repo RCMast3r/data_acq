a0ace387674e4ef1051fc1c85b9151bf9fa8ca1d
ChanceCtrl <StaticElite05@gmail.com>
2024-04-10 23:17:38 +0000

    update input to expect sync byte and fixed message length

--- py_data_acq/py_data_acq/io_handler/serial_handle.py
+++ py_data_acq/py_data_acq/io_handler/serial_handle.py
@@ -4,6 +4,14 @@ from ..common import protobuf_helpers
 from ..common.common_types import QueueData
 
 
+def check_sync_byte(reader):
+    byte_guy = reader.read(1)
+    if byte_guy == 0xFA:
+        return True
+    else:
+        return False
+
+
 async def serial_reciever(can_db: cantools.db.Database, message_classes, q1, q2):
     # Start asyncio on the port
     reader, writer = await serial_asyncio.open_serial_connection(
@@ -12,27 +20,28 @@ async def serial_reciever(can_db: cantools.db.Database, message_classes, q1, q2)
 
     while True:
         try:
-            # Wait for the next message from the buffer, then break it into parts using the byte value for ","
-            nl_byte = await reader.readline()
-            vals = nl_byte.split(b",")
-            frameid = int.from_bytes(vals[0], byteorder="little")
-            msg = can_db.get_message_by_frame_id(frameid)
-            # Break down message
-            decoded_msg = can_db.decode_message(
-                frameid, vals[1], decode_containers=True
-            )
+            if check_sync_byte(reader):
+                # Wait for the next message from the buffer, then break it into parts using the byte value for ","
+                sync_msg = await reader.readexactly(12)
+                frameid = int.from_bytes(sync_msg[0:4], byteorder="little")
+                msg = can_db.get_message_by_frame_id(frameid)
 
-            # Package as protobuf guy
-            msg = protobuf_helpers.pack_protobuf_msg(
-                decoded_msg, msg.name.lower(), message_classes
-            )
-            data = QueueData(msg.DESCRIPTOR.name, msg)
-            # Throw data into queues and start again
-            await q1.put(data)
-            await q2.put(data)
-            # except:
-            #     print("Fail")
-            #     pass
+                # Break down message
+                decoded_msg = can_db.decode_message(
+                    frameid, sync_msg[4:12], decode_containers=True
+                )
+
+                # Package as protobuf guy
+                msg = protobuf_helpers.pack_protobuf_msg(
+                    decoded_msg, msg.name.lower(), message_classes
+                )
+                data = QueueData(msg.DESCRIPTOR.name, msg)
+                # Throw data into queues and start again
+                await q1.put(data)
+                await q2.put(data)
+                # except:
+                #     print("Fail")
+                #     pass
         except (KeyError, TypeError, ValueError, IndexError) as e:
             print(f"Error decoding frame, error : {e}")
             continue
