934a3ab9ffd4989f234a2f79c196a4cad51537ab
Kevin Lou <kevinlou2005@gmail.com>
2024-04-29 21:32:08 -0400

    fixed addr toggle

--- frontend/src/Pages/MCAPRecorder.jsx
+++ frontend/src/Pages/MCAPRecorder.jsx
@@ -23,15 +23,15 @@ export function MCAPRecorder({}) {
     }, [useLocalhost])
 
     async function updateFields() {
-//        const fetchResponse = await fetch(getURL('fields', useLocalhost), {
-//            method: 'GET',
-//            headers: {
-//                Accept: 'application/json',
-//                'Content-Type': 'application/json'
-//            }
-//        })
-//        const json = await fetchResponse.text()
-        const json = '[{"name":"driver","displayName":"Driver","type":"string","automatic":false,"options":["Shayan","Ryan"]},{"name":"testingGoal","displayName":"Testing Goal","type":"string","automatic":false,"options":[]},{"name":"eventType","displayName":"Event Type","type":"string","automatic":false,"options":["Skidpad","Acceleration","Hairpin","SCC Autocross"]},{"name":"startTime","displayName":"Start Time & Date","type":"string","automatic":false,"options":[]},{"name":"endTime","displayName":"End Time & Date","type":"string","automatic":false,"options":[]},{"name":"location","displayName":"Location","type":"string","automatic":false,"options":[]},{"name":"conditions","displayName":"Conditions (ie dry, night time)","type":"string","automatic":false,"options":[]},{"name":"temperature","displayName":"Temperature (C)","type":"string","automatic":false,"options":[]},{"name":"aeroType","displayName":"Aero Type","type":"string","automatic":false,"options":[]},{"name":"MCUversion","displayName":"MCU Version","type":"string","automatic":false,"options":[]},{"name":"yawPIDValues","displayName":"Yaw PID Values","type":"pid","automatic":false,"options":[]},{"name":"TCSPIDValues","displayName":"TCS PID Values","type":"pid","automatic":false,"options":[]},{"name":"TCSEnable","displayName":"TCS Enabled","type":"boolean","automatic":false,"options":[]},{"name":"yawPIDEnable","displayName":"Yaw PID Enabled","type":"boolean","automatic":false,"options":[]},{"name":"launchEnable","displayName":"Launch Enabled","type":"boolean","automatic":false,"options":[]},{"name":"normForceEnable","displayName":"Norm Force Enabled","type":"boolean","automatic":false,"options":[]},{"name":"mechPowerLimEnable","displayName":"Mech Power Lim. Enabled","type":"boolean","automatic":false,"options":[]},{"name":"pidPowerLimEnable","displayName":"PID Power Lim. Enabled","type":"boolean","automatic":false,"options":[]},{"name":"notes","displayName":"Notes","type":"string","automatic":false,"options":[]},{"name":"errors","displayName":"Errors","type":"string","automatic":false,"options":[]}]'
+        const fetchResponse = await fetch(getURL('fields', useLocalhost), {
+            method: 'GET',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        })
+        const json = await fetchResponse.text()
+//        const json = '[{"name":"driver","displayName":"Driver","type":"string","automatic":false,"options":["Shayan","Ryan"]},{"name":"testingGoal","displayName":"Testing Goal","type":"string","automatic":false,"options":[]},{"name":"eventType","displayName":"Event Type","type":"string","automatic":false,"options":["Skidpad","Acceleration","Hairpin","SCC Autocross"]},{"name":"startTime","displayName":"Start Time & Date","type":"string","automatic":false,"options":[]},{"name":"endTime","displayName":"End Time & Date","type":"string","automatic":false,"options":[]},{"name":"location","displayName":"Location","type":"string","automatic":false,"options":[]},{"name":"conditions","displayName":"Conditions (ie dry, night time)","type":"string","automatic":false,"options":[]},{"name":"temperature","displayName":"Temperature (C)","type":"string","automatic":false,"options":[]},{"name":"aeroType","displayName":"Aero Type","type":"string","automatic":false,"options":[]},{"name":"MCUversion","displayName":"MCU Version","type":"string","automatic":false,"options":[]},{"name":"yawPIDValues","displayName":"Yaw PID Values","type":"pid","automatic":false,"options":[]},{"name":"TCSPIDValues","displayName":"TCS PID Values","type":"pid","automatic":false,"options":[]},{"name":"TCSEnable","displayName":"TCS Enabled","type":"boolean","automatic":false,"options":[]},{"name":"yawPIDEnable","displayName":"Yaw PID Enabled","type":"boolean","automatic":false,"options":[]},{"name":"launchEnable","displayName":"Launch Enabled","type":"boolean","automatic":false,"options":[]},{"name":"normForceEnable","displayName":"Norm Force Enabled","type":"boolean","automatic":false,"options":[]},{"name":"mechPowerLimEnable","displayName":"Mech Power Lim. Enabled","type":"boolean","automatic":false,"options":[]},{"name":"pidPowerLimEnable","displayName":"PID Power Lim. Enabled","type":"boolean","automatic":false,"options":[]},{"name":"notes","displayName":"Notes","type":"string","automatic":false,"options":[]},{"name":"errors","displayName":"Errors","type":"string","automatic":false,"options":[]}]'
 
         return JSON.parse(json)
     }
@@ -40,6 +40,7 @@ export function MCAPRecorder({}) {
         <>
             <main>
                 <div className={"flex flex-col gap-4 items-center justify-center pt-6"}>
+                    <AddrToggle useLocalhost={useLocalhost} setUseLocalhost={setUseLocalhost}/>
                     <div className={"flex"}>
                         <PageTitle/>
                     </div>
